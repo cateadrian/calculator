07791a7b23cbb850bd91aff76bd7093f93d7dc42
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-25 08:23:43 -0600

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
 function add(a, b){
     return a + b;
 }
@@ -14,16 +18,25 @@ function divide(a, b){
     return a / b;
 }
 
- 
+function operate(firstNum, secondNum, operator){
+    switch (operator){
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+    };
+}
+
 // Test cases
-console.log(add(1,2)); // 3 
-console.log(add(0,0)); // 0
-
-console.log(subtract(4,2)); // 2
-console.log(subtract (5,10)); // -5
-
-console.log(multiply(1,2)); // 2
-console.log(multiply(10,0)); // 0
-
-console.log(divide(2,0)); // Infinity
-console.log(divide (16,4)); // 4
+console.log(operate(1,2, "+")); // 3 
+console.log(operate(0,0, "+")); // 0
+console.log(operate(4,2, "-")); // 2
+console.log(operate(5,10, "-")); // -5
+console.log(operate(1,2, "*")); // 2
+console.log(operate(10,0, "*")); // 0
+console.log(operate(2,0, "/")); // Infinity
+console.log(operate(16,4, "/")); // 4
